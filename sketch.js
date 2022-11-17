c160668637d718d73f2388fcc3f3f68f05759360
clinton-van-arnam <113942027+vanarnamc@users.noreply.github.com>
2022-11-16 19:31:09 -0500

    margin

--- sketch.js
+++ sketch.js
@@ -3,6 +3,10 @@ let gridWidthSpacing= 15;
 let gridHeightSpacing= 15;
 let volumeAmp= 1800;
 let level = 10;
+let margin= 10; //margin for grid the smaller the number the bigger the margin
+let wMargin;
+let hMargin;
+
 function preload() {  
   song = loadSound('audio/KenyonMillFalls.mp3');
 }
@@ -13,8 +17,8 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   amp = new p5.Amplitude();
   angleMode(DEGREES);
-
-
+  wMargin= width/margin;
+   hMargin=height/margin;
 }
 
 function draw() {
@@ -22,8 +26,8 @@ function draw() {
   let vol = amp.getLevel() * level;
   noFill();
   //ellipse(windowWidth/2, windowHeight/2, vol*volumeAmp, vol*volumeAmp);
- for (let w=0; w<width; w+=gridWidthSpacing){
-  for (let h=0; h<height; h+=gridHeightSpacing){
+ for (let w=0+wMargin; w<width-wMargin; w+=gridWidthSpacing){
+  for (let h=0+hMargin; h<height-hMargin; h+=gridHeightSpacing){
     circle(w,h,random(2,300)*vol);
 
   }
